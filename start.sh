3a5aeea4658f1c84eaa14233d09854cabd12baf3
luofeng7 <80332562+luofeng7@users.noreply.github.com>
2023-10-17 23:43:28 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-
-# 替换 API 地址及备案号
-find '/app' -name '*.js' -exec sed -i -e 's,https://api-hot.imsyy.top,'"$API"',g' {} \;
-find '/app' -name '*.js' -exec sed -i -e 's,豫ICP备2022018134号-1,'"$ICP"',g' {} \;
-
-# # 启动 httpd 服务器
-httpd -f -p 80 -h /app
